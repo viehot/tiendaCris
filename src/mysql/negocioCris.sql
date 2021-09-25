d2da73056ec34f60efa2c7ce485bbb8037d19f18
VieDev <piri.viejo@gmail.com>
2021-09-25 15:54:31 -0300

    correcion script mysql

--- src/mysql/negocioCris.sql
+++ src/mysql/negocioCris.sql
@@ -10,11 +10,11 @@ CREATE TABLE producto (
     descripcion VARCHAR(255) NULL,
     id_marca INT NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY (id_marca) REFERENCES marca(id_mar)
+    FOREIGN KEY (id) REFERENCES marca(id_mar)
 );
 
 CREATE TABLE marca (
-	id_mar INTEGER UNSIGNED AUTO_INCREMENT,
+    id_mar INTEGER UNSIGNED AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
     PRIMARY KEY(id_mar)
 );
@@ -23,6 +23,6 @@ INSERT INTO marca VALUES (1,'GENIUS');
 INSERT INTO marca VALUES (2,'ASUS');
 INSERT INTO marca VALUES (3,'HP');
 
-INSERT INTO producto VALUES (100,'Teclado RGB',10,1800.00,'Un Teclado para niños ratas',1);
-INSERT INTO producto VALUES (150,'Placa Video',2,200000.00,'Para MinaR',2);
-INSERT INTO producto VALUES (145,'Auris De Conejo',15,3000.00,NULL,1);
+INSERT INTO producto VALUES (1,'Teclado RGB',10,1800.00,'Un Teclado para niños ratas',1);
+INSERT INTO producto VALUES (2,'Placa Video',2,200000.00,'Para MinaR',2);
+INSERT INTO producto VALUES (3,'Auris De Conejo',15,3000.00,NULL,1);
